e540250437493e2978e7e22c7730e28171e6968d
Ayan Ansari <1100zaid@gmail.com>
2021-10-18 19:24:43 +0530

    changes

--- src/components/Basics/MenuCard.jsx
+++ src/components/Basics/MenuCard.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 
-const MenuCard = () => {
+const MenuCard = ({ menuData }) => {
   return (
-    <div className="card-container">
-      <div className="card">
-        <div className="card-body">
-          <span className="card-number card-circle subtle">1</span>
-          <span className="card-author subtle">Breakfast</span>
-          <h2 className="card-title">Maggi</h2>
-          <span className="card-description subtle">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-            illum perspiciatis tempore repellendus omnis error a rem, nisi
-            repellat cupiditate?Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Optio, voluptatum!
-          </span>
-          <div className="card-read">Read</div>
-        </div>
-        {/* <img src={image} alt="image" className="card-media" /> */}
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((currEle) => {
+          return (
+            <>
+              <div className="card-container" key={currEle.id}>
+                <div className="card">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">
+                      {currEle.id}
+                    </span>
+                    <span className="card-author subtle">
+                      {currEle.category}
+                    </span>
+                    <h2 className="card-title">{currEle.name}</h2>
+                    <span className="card-description subtle">
+                      {currEle.description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={currEle.image} alt="images" className="card-media" />
 
-        <span className="card-tag subtle">Order Now</span>
-      </div>
-    </div>
+                  <span className="card-tag subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
   );
 };
 
--- src/components/Basics/Restaurant.jsx
+++ src/components/Basics/Restaurant.jsx
@@ -6,9 +6,50 @@ import "./style.css";
 function Restaurant() {
   const [menuData, setMenuData] = useState(Menu);
 
+  const filterItem = (category) => {
+    const updatedList = Menu.filter((curEle) => {
+      return curEle.category === category;
+    });
+    setMenuData(updatedList);
+  };
+
   return (
     <>
-      <MenuCard />
+      <nav className="navbar">
+        <div className="btn-group">
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("breakfast")}
+          >
+            Breakfast
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("lunch")}
+          >
+            Lunch
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("evening")}
+          >
+            Evening
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("dinner")}
+          >
+            Dinner
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("all")}
+          >
+            All
+          </button>
+        </div>
+      </nav>
+      <MenuCard menuData={menuData} />
     </>
   );
 }
